b1da8fc819c9efe4302ad1a009ac7ab84dc8e47e
Wilker Lucio <wilkerlucio@gmail.com>
2014-02-26 02:35:29 -0300

    added FutureWorker and improved others

--- test/future_goodies_test.dart
+++ test/future_goodies_test.dart
@@ -18,11 +18,11 @@ void runTests() {
         testValidSequence([], []);
       });
 
-      test("resolves a list with promises results", () {
+      test("resolves a list with future results", () {
         testValidSequence([1, 2], [3, 4], mapFunction: plusTwo);
       });
 
-      test("rejects when a promise rejects", () {
+      test("rejects when a future rejects", () {
         expect(sequence([1], throwError), throws);
       });
 
@@ -37,19 +37,21 @@ void runTests() {
           return lastCompleter.future;
         });
 
-        expect(calls, equals([1]));
+        return new Future.microtask(() {
+          expect(calls, equals([1]));
 
-        lastCompleter.complete(calls.last);
+          lastCompleter.complete(calls.last);
 
-        return lastCompleter.future.then((v) {
-          expect(calls, equals([1, 2]));
+          return lastCompleter.future.then((v) {
+            expect(calls, equals([1, 2]));
+          });
         });
       });
     });
 
     group("pipeline", () {
-      test("resolves to null when input is blank", () {
-        expect(pipeline(null, [], (acc, value) {}), completion(isNull));
+      test("resolves to initial when input is blank", () {
+        expect(pipeline(1, [], (acc, value) {}), completion(1));
       });
 
       test("resolves to the returned value", () {
@@ -73,7 +75,109 @@ void runTests() {
         expect(silentError(completer.future), completion(isNull));
       });
     });
+
+    group('FutureWorker', () {
+      test('runs a job direct when the worker count is under limit', () {
+        FutureWorker worker = new FutureWorker(1);
+        SimpleJob job = new SimpleJob();
+
+        Future<String> future = worker.push(job.run);
+
+        expect(job.started, isTrue);
+
+        job.completer.complete('test');
+
+        expect(future, completion('test'));
+      });
+
+      test('doesn\'t run if the workers are full', () {
+        FutureWorker worker = new FutureWorker(1);
+
+        SimpleJob secondJob = new SimpleJob();
+
+        worker.push(new SimpleJob().run);
+        worker.push(secondJob.run);
+
+        expect(secondJob.started, isFalse);
+      });
+
+      test('run the job when the queue has added limit', () {
+        FutureWorker worker = new FutureWorker(1);
+
+        SimpleJob job = new SimpleJob();
+        SimpleJob secondJob = new SimpleJob();
+
+        Future future = worker.push(job.run);
+        worker.push(secondJob.run);
+
+        expect(secondJob.started, isFalse);
+
+        job.completer.complete(null);
+
+        return future.then((value) {
+          expect(secondJob.started, isTrue);
+        });
+      });
+
+      test('run the job when the queue has added limit for a failed task', () {
+        FutureWorker worker = new FutureWorker(1);
+
+        SimpleJob job = new SimpleJob();
+        SimpleJob secondJob = new SimpleJob();
+
+        Future future = worker.push(job.run);
+        worker.push(secondJob.run);
+
+        expect(secondJob.started, isFalse);
+
+        job.completer.completeError(new Exception('bla'));
+
+        return future.catchError((err) {
+          expect(secondJob.started, isTrue);
+        });
+      });
+
+      test('run all the jobs', () {
+        FutureWorker worker = new FutureWorker(1);
+
+        SimpleJob job = new SimpleJob();
+        SimpleJob secondJob = new SimpleJob();
+
+        worker.push(job.run);
+        worker.push(secondJob.run);
+
+        job.completer.complete('one');
+        secondJob.completer.complete('two');
+
+        expect(secondJob.completer.future, completion('two'));
+      });
+
+      test('timeout when asked', () {
+        FutureWorker worker = new FutureWorker(1, timeout: new Duration(milliseconds: 10));
+
+        SimpleJob job = new SimpleJob();
+        SimpleJob secondJob = new SimpleJob();
+
+        silentError(worker.push(job.run));
+        silentError(worker.push(secondJob.run));
+
+        return new Future.delayed(new Duration(milliseconds: 50), () {
+          expect(secondJob.started, isTrue);
+        });
+      });
+    });
   });
+}
+
+class SimpleJob {
+  bool started = false;
+  Completer completer = new Completer();
+
+  Future run() {
+    started = true;
+
+    return completer.future;
+  }
 }
 
 Future plusTwo(v) => new Future.value(v + 2);
